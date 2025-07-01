6587d8669af6c8ff43d44f6b292afea7233f0202
helmi <e-m.helmi@lean.sa>
2025-07-01 21:26:38 +0300

    git commit -m "initial commit"

---
+++ app/build.gradle.kts
@@ -0,0 +1,80 @@
+plugins {
+  alias(libs.plugins.android.application)
+  alias(libs.plugins.kotlin.android)
+  alias(libs.plugins.kotlin.compose)
+  alias(libs.plugins.hilt)
+//  alias(libs.plugins.kaptPlugin)
+  kotlin("kapt")
+}
+
+android {
+  namespace = "com.github.mhelmi.thmanyahtask"
+  compileSdk = 35
+
+  defaultConfig {
+    applicationId = "com.github.mhelmi.thmanyahtask"
+    minSdk = 24
+    targetSdk = 35
+    versionCode = 1
+    versionName = "1.0"
+
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    buildConfigField("String", "BASE_URL", "\"https://api-v2-b2sit6oh3a-uc.a.run.app/\"")
+  }
+
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+    }
+  }
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+  }
+  kotlinOptions {
+    jvmTarget = "11"
+  }
+  buildFeatures {
+    buildConfig = true
+    compose = true
+  }
+  composeOptions {
+    kotlinCompilerExtensionVersion = "1.5.1"
+  }
+}
+
+dependencies {
+
+  implementation(libs.androidx.core.ktx)
+  implementation(libs.androidx.lifecycle.runtime.ktx)
+  implementation(libs.androidx.activity.compose)
+  implementation(platform(libs.androidx.compose.bom))
+  implementation(libs.androidx.ui)
+  implementation(libs.androidx.ui.graphics)
+  implementation(libs.androidx.ui.tooling.preview)
+  implementation(libs.androidx.material3)
+  implementation(libs.androidx.navigation.compose)
+  implementation(libs.androidx.hilt.navigation.compose)
+  implementation(libs.kotlinx.coroutines.android)
+  implementation(libs.coil)
+  implementation(libs.coil.network.okhttp)
+  implementation(libs.retrofit)
+  implementation(libs.converter.gson)
+  implementation(libs.androidx.paging.runtime.ktx)
+  implementation(libs.androidx.paging.compose)
+
+
+  // Hilt Dependencies
+  implementation(libs.hilt.android)
+  kapt(libs.hilt.android.compiler)
+
+  testImplementation(libs.junit)
+  androidTestImplementation(libs.androidx.junit)
+  androidTestImplementation(libs.androidx.espresso.core)
+  androidTestImplementation(platform(libs.androidx.compose.bom))
+  androidTestImplementation(libs.androidx.ui.test.junit4)
+  debugImplementation(libs.androidx.ui.tooling)
+  debugImplementation(libs.androidx.ui.test.manifest)
+}
---
+++ app/src/main/java/com/github/mhelmi/thmanyahtask/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.github.mhelmi.thmanyahtask
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.github.mhelmi.thmanyahtask.ui.theme.ThmanyahTaskTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContent {
+      ThmanyahTaskTheme {
+
+      }
+    }
+  }
+}
